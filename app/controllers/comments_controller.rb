9b95bf51bf674748528bfc3191c57dff36bf36eb
Karen Bacilio <karen.bacilio@gmail.com>
2021-05-05 20:11:34 -0400

    Changed/Created in updating-associated-record

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,28 @@
 class CommentsController < ApplicationController
+    before_action :set_post
     def create
-        @post = Post.find(params[:post_id])
-        # Create associated model, just like we did in the console before
         @comment = @post.comments.create(comment_params)
         # We want to show the comment in the context of the Post
         redirect_to @post
-      end
-      private
-      def comment_params
+    end
+
+    def edit
+        @comment = Comment.find(params[:id])
+    end
+
+    def update
+        @comment = Comment.find(params[:id])
+        @comment.update(comment_params)
+        redirect_to @post # @post will be set by before_action
+    end
+
+    private
+    def comment_params
         params.require(:comment).permit(:name, :content)
-      end
+    end
+
+    def set_post
+        @post = Post.find(params[:post_id])
+    end
   
 end
